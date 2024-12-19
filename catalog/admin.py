4b962127f8fd0079d2d805a7d73c97bdb525b212
Anton Fomenko <antonfomenko1234@gmail.com>
2024-12-19 18:42:39 +0300

    some changes

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,12 +4,12 @@ from .models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'description')
     search_fields = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category', 'lust_change_date')
     list_filter = ('category',)
     search_fields = ('name', 'description')
--- catalog/models.py
+++ catalog/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=150, verbose_name='Название группы')
-    description = models.TextField(verbose_name='Описание группы')
+    name = models.CharField(max_length=150, verbose_name='Название категории')
+    description = models.TextField(verbose_name='Описание категории')
 
     def __str__(self):
         return self.name
@@ -20,7 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='имя')
     description = models.TextField(verbose_name='описание')
     image = models.ImageField(upload_to='photos/', verbose_name='фотография', null=True, blank=True, default=None)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='название группы')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='название категории')
     price = models.IntegerField(verbose_name='цена')
     create_date = models.DateField(auto_now=True, verbose_name='дата создания')
     lust_change_date = models.DateField(auto_now_add=True, verbose_name='дата последнего изменения')
