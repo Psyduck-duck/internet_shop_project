b2634bc76fec39fcbff5bb2343a5be393c566513
Anton Fomenko <antonfomenko1234@gmail.com>
2025-01-23 15:02:31 +0300

    rework to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product, Contact
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import ListView, DetailView, View, TemplateView
+
+from catalog.models import Product
 
 
-def show_home(request):
-    products = Product.objects.all()
-    return render(request, 'catalog/home.html', {'products': products})
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
 
 
-def show_contacts(request):
-    contacts = Contact.objects.all()
-    if request.method == 'POST':
+class ProductDetailView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class ContactView(View):
+    model = Contact
+    template_name = 'catalog/contacts.html'
+    context_object_name = 'contacts'
+
+    def get(self, request):
+        contacts = Contact.objects.all()
+        return render(request, 'catalog/contacts.html', {'contacts': contacts})
+
+    def post(self, request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         return HttpResponse(f'Спасибо, {name}! Ваше сообщение принято.')
-    return render(request, 'catalog/contacts.html', {'contacts': contacts})
-
-
-def show_product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {
-        'product': product
-    }
-    return render(request, 'catalog/product_detail.html', context)
