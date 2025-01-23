fb4a96fd645ab8984b99512e41e48e69afe864bb
Anton Fomenko <antonfomenko1234@gmail.com>
2025-01-23 15:51:01 +0300

    create views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'is_published', 'views']
+    template_name = 'students/blog_form.html'
+    success_url = reverse_lazy('students:blog_list')
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'blogs'
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'blog'
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'is_published', 'views']
+    template_name = 'students/blog_form.html'
+    success_url = reverse_lazy('students:blog_list')
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog/blog_delete.html'
+    success_url = reverse_lazy('students:blog_list')
