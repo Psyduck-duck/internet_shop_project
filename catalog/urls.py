4681dbe119eba2598e239d67597c97228dd353c6
Anton Fomenko <antonfomenko1234@gmail.com>
2025-02-03 20:02:12 +0300

    create CRUD with forms for products

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,11 @@ app_name = 'catalog'
 
 
 urlpatterns = [
-    path('home/', views.ProductListView.as_view(), name='home'),
+    path('home/', views.ProductHomeListView.as_view(), name='home'),
+    path('list/', views.ProductsListView.as_view(), name='list'),
+    path('create/', views.ProductCreateView.as_view(), name='create'),
+    path('update/<int:pk>', views.ProductUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.ProductDeleteView.as_view(), name='delete'),
     path('contacts/', views.ContactView.as_view(), name='contacts'),
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+
 from .models import Product, Contact
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import ListView, DetailView, View, CreateView, UpdateView, DeleteView
+from .forms import ProductForm
 
 from catalog.models import Product
 
 
-class ProductListView(ListView):
+class ProductHomeListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
     context_object_name = 'products'
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'catalog/products_list.html'
+    context_object_name = 'products'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'catalog/product_form.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'catalog/product_form.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_delete.html'
+    success_url = reverse_lazy('catalog:list')
+    context_object_name = 'product'
 
 
 class ProductDetailView(DetailView):
