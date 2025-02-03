f68b441352987f95b3aedd7343dbeb54106134a9
Anton Fomenko <antonfomenko1234@gmail.com>
2025-02-03 21:13:45 +0300

    create Task 1

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,26 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        bad_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        if name.lower() in bad_words:
+            raise ValidationError(f'Name не может содержать значение {name}')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        bad_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                     'радар']
+        if description.lower() in bad_words:
+            raise ValidationError(f'Name не может содержать значение {description}')
+        return description
+
     class Meta:
         model = Product
         fields = ['name', 'description', 'category', 'price']
